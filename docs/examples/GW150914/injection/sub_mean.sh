5404d450d4288505201d341c89b066dfd45b45d8
Sizheng Ma <sma@caltech.edu>
2022-07-11 15:45:52 -0700

    mean injection

---
+++ docs/examples/GW150914/injection/sub_mean.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+#SBATCH -J 7
+#SBATCH -o rest/7out
+#SBATCH -e rest/error.txt
+#SBATCH -N 1
+#SBATCH -n 24
+#SBATCH -t 24:00:00
+#SBATCH --mail-user=sma@caltech.edu
+#SBATCH --mail-type=END
+##SBATCH -p debug
+
+source activate ringdown
+~/.conda/envs/ringdown/bin/python3 mean.py
